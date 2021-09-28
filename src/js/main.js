277bdfdec2bd49114ab014b82d4acc538af9ae6a
Denzol255 <den-zoloto@mail.ru>
2021-09-28 19:24:07 +0300

    Added: start block w/ mobile

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,14 @@
+import AOS from 'aos';
 import '../index.html';
+import 'aos/dist/aos.css';
 import '../scss/style.scss';
 import { webpChecker } from './modules/functions';
 import clickListner from './modules/clickListner';
 import sendForm from './modules/sendForm';
 import spollers from './modules/spollers';
+
+// AOS init
+AOS.init();
 
 // Checking the browser for webp support
 webpChecker();
